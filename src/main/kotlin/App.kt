1e0f8a49dee0954f90f1924f9a0f535bba80010a
Mike Kosmatopoulos <stonos@gmail.com>
2020-05-15 13:31:48 +0300

    Initial DebugPanel implementation

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,19 +1,41 @@
-import components.receiver
+import cast.castMediaPlayer
+import components.Stats
+import components.debugPanel
+import kotlinx.css.*
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import styled.css
+import styled.styledDiv
 
 external interface AppState : RState {
-
+    var logs: List<String>
+    var stats: Stats
+    var debugEnabled: Boolean
 }
 
 class App : RComponent<RProps, AppState>() {
     override fun AppState.init() {
-
+        logs = listOf()
+        stats = Stats(0, "INIT", 0)
+        debugEnabled = true
     }
 
     override fun RBuilder.render() {
-        receiver { }
+        styledDiv {
+            css {
+                backgroundColor = Color.black
+                color = Color.white
+                height = 100.pct
+                textAlign = TextAlign.left
+            }
+            debugPanel {
+                logs = state.logs
+                stats = state.stats
+                debugEnabled = state.debugEnabled
+            }
+            castMediaPlayer { }
+        }
     }
 }
---
+++ src/main/kotlin/components/DebugPanel.kt
@@ -0,0 +1,84 @@
+package components
+
+import kotlinx.css.*
+import kotlinx.css.properties.lh
+import react.*
+import react.dom.div
+import styled.css
+import styled.styledDiv
+import styled.styledSpan
+
+data class Stats(val bitrate: Int, val state: String, val currentMediaTime: Int)
+
+external interface DebugPanelProps : RProps {
+    var logs: List<String>
+    var stats: Stats
+    var debugEnabled: Boolean
+}
+
+class DebugPanel : RComponent<DebugPanelProps, RState>() {
+    override fun RBuilder.render() {
+        styledDiv {
+            css {
+                display = if (props.debugEnabled) Display.block else Display.none
+                height = 100.pct
+                width = 50.pct
+                fontFamily = "monospace"
+                paddingLeft = 10.px
+                paddingTop = 10.px
+                position = Position.absolute
+                top = 0.px
+                left = 0.px
+                zIndex = 1
+            }
+
+            styledDiv { //TODO: Extract to a debug-stats component
+                css {
+                    textAlign = TextAlign.left
+                    width = 100.pct
+                    height = 20.pct
+                }
+
+                div { +"bandwidth: ${props.stats.bitrate}" }
+                div { +"state: ${props.stats.state}" }
+                div { +"current time: ${props.stats.currentMediaTime}" }
+            }
+
+            styledDiv {
+                css {
+                    overflow = Overflow.auto
+                    textAlign = TextAlign.left
+                    width = 100.pct
+                    height = 75.pct
+                    wordWrap = WordWrap.breakWord
+                }
+
+                props.logs.forEachIndexed { index, log ->
+                    styledDiv {
+                        css {
+                            borderBottom = "1px solid #fff3"
+                            lineHeight = 1.4.em.lh
+                            margin(0.px)
+                            padding(0.em, 0.5.em)
+                            width = 100.pct
+                        }
+                        key = index.toString()
+                        styledSpan {
+                            css {
+                                paddingRight = 0.5.em
+                                whiteSpace = WhiteSpace.preWrap
+                            }
+                            +log
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun RBuilder.debugPanel(handler: DebugPanelProps.() -> Unit): ReactElement {
+    return child(DebugPanel::class) {
+        this.attrs(handler)
+    }
+}
